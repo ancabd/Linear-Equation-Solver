3af3735c91fd60f3e7b06c3dba10b8c60faa73bb
ancabd <ancabd@gmail.com>
2021-11-24 14:59:29 +0100

    Added files

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace rezolvare_sisteme_liniare_determinanti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(303, 117);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(272, 359);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkKhaki;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Location = new System.Drawing.Point(585, 421);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(585, 117);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(233, 294);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(300, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ecuations:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(593, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Solutions:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(282, 469);
+            this.label3.TabIndex = 5;
+            this.label3.Text = resources.GetString("label3.Text");
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(304, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(515, 37);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(832, 490);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Sitka Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Rezolvare de sisteme liniare";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rezolvare_sisteme_liniare_determinanti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int nrvariables = 0, n = 1;
+        int[] variables, result;
+        int[,] matrix;
+        char[] charvariables;
+
+        bool ifDigit(char x)
+        {
+            return x <= '9' && x >= '0';
+        }
+        bool ifLetter(char x)
+        {
+            return (x <= 'z' && x >= 'a') || (x <= 'Z' && x >= 'A');
+        }
+
+        int letterCode(char x)
+        {
+            if (x <= 'z' && x >= 'a') return x - 'a' + 1;
+            else return x - 'A' + 30;
+        }
+
+        void CreateMatrix()
+        {
+            try
+            {
+                variables = new int[60];
+                result = new int[60];
+                charvariables = new char[60];
+                matrix = new int[60, 60];
+                nrvariables = 0; n = 1;
+
+                string arr;
+                arr = textBox1.Text;
+                for (int i = 0; i < arr.Length; i++)
+                    if (ifLetter(arr[i]) && variables[letterCode(arr[i])] == 0)
+                    {
+                        variables[letterCode(arr[i])] = ++nrvariables;
+                        charvariables[nrvariables] = arr[i];
+                    }
+
+                /// calulam matricea
+                bool beforeEq = true;
+                int nr = 0, sign = 1;
+                for (int i = 0; i < arr.Length; i++, n++)
+                {
+                    beforeEq = true;
+                    sign = 1;
+                    nr = 0;
+                    while (i < arr.Length && arr[i] != '\n')
+                    {
+                        if (ifDigit(arr[i]))
+                            nr = nr * 10 + arr[i] - '0';
+                        else if (ifLetter(arr[i]))
+                        {
+                            if (nr == 0) nr = 1;
+                            if (beforeEq) matrix[n, variables[letterCode(arr[i])]] += nr * sign;
+                            else matrix[n, variables[letterCode(arr[i])]] -= nr * sign;
+                            nr = 0;
+                        }
+                        else if (arr[i] == '+')
+                        {
+                            if (nr != 0)
+                            {
+                                if (beforeEq) result[n] += nr * sign;
+                                else result[n] -= nr * sign;
+                                nr = 0;
+                            }
+                            sign = 1;
+                        }
+                        else if (arr[i] == '-')
+                        {
+                            if (nr != 0)
+                            {
+                                if (beforeEq) result[n] += nr * sign;
+                                else result[n] -= nr * sign;
+                                nr = 0;
+                            }
+                            sign = -1;
+                        }
+                        else if (arr[i] == '=')
+                        {
+                            if (nr != 0)
+                            {
+                                if (beforeEq) result[n] += nr * sign;
+                                else result[n] -= nr * sign;
+                                nr = 0;
+                            }
+                            beforeEq = false;
+                            sign = 1;
+                        }
+                        i++;
+                    }
+                    if (nr != 0)
+                    {
+                        if (beforeEq) result[n] += nr * sign;
+                        else result[n] -= nr * sign;
+                        nr = 0;
+                    }
+                }
+                for (int i = 1; i <= n; ++i)
+                    result[i] *= -1;
+                n--;
+            }
+            catch
+            {
+                MessageBox.Show("This is not a vald equation");
+            };
+        }
+        
+        int AlgebricComplementary(int[,] x, int i, int j, int m)
+        {
+            int[,] newx = new int[m, m];
+            int i2 = 0, j2 = 0;
+            for (int i1 = 1; i1 <= m; i1++)
+            {
+                j2 = 0;
+                if (i1 != i) i2++;
+                for (int j1 = 1; j1 <= m; j1++)
+                {
+                    if (j1 != j) j2++;
+                    if (i1 != i && j1 != j)
+                        newx[i2, j2] = x[i1, j1];
+                }
+            }
+            if ((i + j) % 2 == 0) return FindDeterminant(newx, m - 1);
+            else return FindDeterminant(newx, m - 1) * -1;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Default");
+            comboBox1.Items.Add("Dark Mode (Red)");
+            comboBox1.Items.Add("Dark Mode (Blue)");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string s = comboBox1.SelectedItem.ToString();
+            switch(s)
+            {
+                case "Default":
+                    this.ForeColor = SystemColors.ControlText;
+                    this.BackColor = SystemColors.ActiveCaption;
+                    button1.BackColor = Color.DarkKhaki;
+                    comboBox1.BackColor = textBox2.BackColor = textBox1.BackColor = SystemColors.Window;
+                    comboBox1.ForeColor = textBox1.ForeColor = textBox2.ForeColor = SystemColors.WindowText;
+                    break;
+                case "Dark Mode (Red)":
+                    this.ForeColor = SystemColors.Window;
+                    this.BackColor = Color.FromArgb(23, 21, 21);
+                    button1.BackColor = Color.FromArgb(125, 30, 42);
+                    comboBox1.BackColor = textBox1.BackColor = textBox2.BackColor = Color.FromArgb(52, 48, 47);
+                    comboBox1.ForeColor = textBox1.ForeColor = textBox2.ForeColor = Color.RosyBrown;
+                    break;
+                case "Dark Mode (Blue)":
+                    this.ForeColor = Color.CornflowerBlue;
+                    this.BackColor = Color.FromArgb(23, 21, 21);
+                    button1.BackColor = Color.MidnightBlue;
+                    comboBox1.BackColor = textBox1.BackColor = textBox2.BackColor = Color.FromArgb(52, 48, 47);
+                    comboBox1.ForeColor = textBox1.ForeColor = textBox2.ForeColor = Color.White;
+                    break;
+            }
+            
+        }
+
+        int FindDeterminant(int[,] x, int m)
+        {
+            int det = 0;
+            if (m == 2) // determinant de ordin 2
+                det = x[1, 1] * x[2, 2] - x[1, 2] * x[2, 1];
+            else if (m == 1) det = x[1, 1];
+            else
+            {
+                //dezvolt dupa linia 1
+                for (int i = 1; i <= m; i++)
+                    det += x[1, i] * AlgebricComplementary(x, 1, i, m);
+            }
+            
+            return det;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateMatrix();
+            try
+            {
+                int det = FindDeterminant(matrix, n);
+                if (det == 0) MessageBox.Show("This is not a vald equation");
+                else
+                {
+                    int[,] newmatrix = new int[60, 60];
+
+                    for (int i = 1; i <= n; i++)
+                        for (int j = 1; j <= n; j++)
+                            newmatrix[i, j] = matrix[i, j];
+
+                    /// folosesc relatiile lui Cramer
+                    int deti;
+                    double xvalue;
+                    string s = "";
+                    for (int j = 1; j <= n; j++)
+                    {
+                        for (int i = 1; i <= n; i++)
+                        {
+                            newmatrix[i, j - 1] = matrix[i, j - 1];
+                            newmatrix[i, j] = result[i];
+                        }
+                        deti = FindDeterminant(newmatrix, n);
+                        xvalue = 1.00 * deti / det;
+                        s += charvariables[j] + "=" + xvalue + "\r\n";
+                    }
+                    textBox2.Text = s;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("This is not a vald equation");
+            }
+            /*string s = "";
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                    s += matrix[i, j] + " ";
+                s += "\r\n";
+            }
+            textBox2.Text = s;*/
+        }
+    }
+}
